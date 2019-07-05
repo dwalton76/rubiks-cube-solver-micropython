280dcb27866d48416f31608c80455a13e0b2c683
Daniel Walton <dwalton76@gmail.com>
2019-07-05 09:04:16 -0400

    rotate() only change the minimum number of squares

--- rubikscubesolvermicropython/cube.py
+++ rubikscubesolvermicropython/cube.py
@@ -1,5 +1,6 @@
 
 from micropython import const
+from rubikscubesolvermicropython.rotate import *
 import utime
 
 U = const(0)
@@ -27,6 +28,36 @@ get_step_string_side2str = {
     3 : "B",
     4 : "R",
     5 : "L",
+}
+
+rotate_map = {
+    "B": rotate_B,
+    "B'": rotate_B_prime,
+    "B2": rotate_B2,
+    "D": rotate_D,
+    "D'": rotate_D_prime,
+    "D2": rotate_D2,
+    "F": rotate_F,
+    "F'": rotate_F_prime,
+    "F2": rotate_F2,
+    "L": rotate_L,
+    "L'": rotate_L_prime,
+    "L2": rotate_L2,
+    "R": rotate_R,
+    "R'": rotate_R_prime,
+    "R2": rotate_R2,
+    "U": rotate_U,
+    "U'": rotate_U_prime,
+    "U2": rotate_U2,
+    "x": rotate_x,
+    "x'": rotate_x_prime,
+    "x2": rotate_x2,
+    "y": rotate_y,
+    "y'": rotate_y_prime,
+    "y2": rotate_y2,
+    "z": rotate_z,
+    "z'": rotate_z_prime,
+    "z2": rotate_z2
 }
 
 cube_layout = """
@@ -72,39 +103,6 @@ rotations_24 = (
     ("x'", "y'"),
     ("x'", "y", "y"),
 )
-
-
-
-# facelet swaps for 3x3x3 moves
-swaps_333 = {
-    "B" : (29, 32, 35, 3, 4, 5, 6, 7, 8, 2, 10, 11, 1, 13, 14, 0, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 53, 30, 31, 52, 33, 34, 51, 42, 39, 36, 43, 40, 37, 44, 41, 38, 45, 46, 47, 48, 49, 50, 9, 12, 15),
-    "B'" : (15, 12, 9, 3, 4, 5, 6, 7, 8, 51, 10, 11, 52, 13, 14, 53, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 0, 30, 31, 1, 33, 34, 2, 38, 41, 44, 37, 40, 43, 36, 39, 42, 45, 46, 47, 48, 49, 50, 35, 32, 29),
-    "B2" : (53, 52, 51, 3, 4, 5, 6, 7, 8, 35, 10, 11, 32, 13, 14, 29, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 15, 30, 31, 12, 33, 34, 9, 44, 43, 42, 41, 40, 39, 38, 37, 36, 45, 46, 47, 48, 49, 50, 2, 1, 0),
-    "D" : (0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 42, 43, 44, 18, 19, 20, 21, 22, 23, 15, 16, 17, 27, 28, 29, 30, 31, 32, 24, 25, 26, 36, 37, 38, 39, 40, 41, 33, 34, 35, 51, 48, 45, 52, 49, 46, 53, 50, 47),
-    "D'" : (0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 24, 25, 26, 18, 19, 20, 21, 22, 23, 33, 34, 35, 27, 28, 29, 30, 31, 32, 42, 43, 44, 36, 37, 38, 39, 40, 41, 15, 16, 17, 47, 50, 53, 46, 49, 52, 45, 48, 51),
-    "D2" : (0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 33, 34, 35, 18, 19, 20, 21, 22, 23, 42, 43, 44, 27, 28, 29, 30, 31, 32, 15, 16, 17, 36, 37, 38, 39, 40, 41, 24, 25, 26, 53, 52, 51, 50, 49, 48, 47, 46, 45),
-    "F" : (0, 1, 2, 3, 4, 5, 17, 14, 11, 9, 10, 45, 12, 13, 46, 15, 16, 47, 24, 21, 18, 25, 22, 19, 26, 23, 20, 6, 28, 29, 7, 31, 32, 8, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 33, 30, 27, 48, 49, 50, 51, 52, 53),
-    "F'" : (0, 1, 2, 3, 4, 5, 27, 30, 33, 9, 10, 8, 12, 13, 7, 15, 16, 6, 20, 23, 26, 19, 22, 25, 18, 21, 24, 47, 28, 29, 46, 31, 32, 45, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 11, 14, 17, 48, 49, 50, 51, 52, 53),
-    "F2" : (0, 1, 2, 3, 4, 5, 47, 46, 45, 9, 10, 33, 12, 13, 30, 15, 16, 27, 26, 25, 24, 23, 22, 21, 20, 19, 18, 17, 28, 29, 14, 31, 32, 11, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 8, 7, 6, 48, 49, 50, 51, 52, 53),
-    "L" : (44, 1, 2, 41, 4, 5, 38, 7, 8, 15, 12, 9, 16, 13, 10, 17, 14, 11, 0, 19, 20, 3, 22, 23, 6, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 51, 39, 40, 48, 42, 43, 45, 18, 46, 47, 21, 49, 50, 24, 52, 53),
-    "L'" : (18, 1, 2, 21, 4, 5, 24, 7, 8, 11, 14, 17, 10, 13, 16, 9, 12, 15, 45, 19, 20, 48, 22, 23, 51, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 6, 39, 40, 3, 42, 43, 0, 44, 46, 47, 41, 49, 50, 38, 52, 53),
-    "L2" : (45, 1, 2, 48, 4, 5, 51, 7, 8, 17, 16, 15, 14, 13, 12, 11, 10, 9, 44, 19, 20, 41, 22, 23, 38, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 24, 39, 40, 21, 42, 43, 18, 0, 46, 47, 3, 49, 50, 6, 52, 53),
-    "R" : (0, 1, 20, 3, 4, 23, 6, 7, 26, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 47, 21, 22, 50, 24, 25, 53, 33, 30, 27, 34, 31, 28, 35, 32, 29, 8, 37, 38, 5, 40, 41, 2, 43, 44, 45, 46, 42, 48, 49, 39, 51, 52, 36),
-    "R'" : (0, 1, 42, 3, 4, 39, 6, 7, 36, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 2, 21, 22, 5, 24, 25, 8, 29, 32, 35, 28, 31, 34, 27, 30, 33, 53, 37, 38, 50, 40, 41, 47, 43, 44, 45, 46, 20, 48, 49, 23, 51, 52, 26),
-    "R2" : (0, 1, 47, 3, 4, 50, 6, 7, 53, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 42, 21, 22, 39, 24, 25, 36, 35, 34, 33, 32, 31, 30, 29, 28, 27, 26, 37, 38, 23, 40, 41, 20, 43, 44, 45, 46, 2, 48, 49, 5, 51, 52, 8),
-    "U" : (6, 3, 0, 7, 4, 1, 8, 5, 2, 18, 19, 20, 12, 13, 14, 15, 16, 17, 27, 28, 29, 21, 22, 23, 24, 25, 26, 36, 37, 38, 30, 31, 32, 33, 34, 35, 9, 10, 11, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53),
-    "U'" : (2, 5, 8, 1, 4, 7, 0, 3, 6, 36, 37, 38, 12, 13, 14, 15, 16, 17, 9, 10, 11, 21, 22, 23, 24, 25, 26, 18, 19, 20, 30, 31, 32, 33, 34, 35, 27, 28, 29, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53),
-    "U2" : (8, 7, 6, 5, 4, 3, 2, 1, 0, 27, 28, 29, 12, 13, 14, 15, 16, 17, 36, 37, 38, 21, 22, 23, 24, 25, 26, 9, 10, 11, 30, 31, 32, 33, 34, 35, 18, 19, 20, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53),
-    "x" : (18, 19, 20, 21, 22, 23, 24, 25, 26, 11, 14, 17, 10, 13, 16, 9, 12, 15, 45, 46, 47, 48, 49, 50, 51, 52, 53, 33, 30, 27, 34, 31, 28, 35, 32, 29, 8, 7, 6, 5, 4, 3, 2, 1, 0, 44, 43, 42, 41, 40, 39, 38, 37, 36),
-    "x'" : (44, 43, 42, 41, 40, 39, 38, 37, 36, 15, 12, 9, 16, 13, 10, 17, 14, 11, 0, 1, 2, 3, 4, 5, 6, 7, 8, 29, 32, 35, 28, 31, 34, 27, 30, 33, 53, 52, 51, 50, 49, 48, 47, 46, 45, 18, 19, 20, 21, 22, 23, 24, 25, 26),
-    "x2" : (45, 46, 47, 48, 49, 50, 51, 52, 53, 17, 16, 15, 14, 13, 12, 11, 10, 9, 44, 43, 42, 41, 40, 39, 38, 37, 36, 35, 34, 33, 32, 31, 30, 29, 28, 27, 26, 25, 24, 23, 22, 21, 20, 19, 18, 0, 1, 2, 3, 4, 5, 6, 7, 8),
-    "y" : (6, 3, 0, 7, 4, 1, 8, 5, 2, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 9, 10, 11, 12, 13, 14, 15, 16, 17, 47, 50, 53, 46, 49, 52, 45, 48, 51),
-    "y'" : (2, 5, 8, 1, 4, 7, 0, 3, 6, 36, 37, 38, 39, 40, 41, 42, 43, 44, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 51, 48, 45, 52, 49, 46, 53, 50, 47),
-    "y2" : (8, 7, 6, 5, 4, 3, 2, 1, 0, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 53, 52, 51, 50, 49, 48, 47, 46, 45),
-    "z" : (15, 12, 9, 16, 13, 10, 17, 14, 11, 51, 48, 45, 52, 49, 46, 53, 50, 47, 24, 21, 18, 25, 22, 19, 26, 23, 20, 6, 3, 0, 7, 4, 1, 8, 5, 2, 38, 41, 44, 37, 40, 43, 36, 39, 42, 33, 30, 27, 34, 31, 28, 35, 32, 29),
-    "z'" : (29, 32, 35, 28, 31, 34, 27, 30, 33, 2, 5, 8, 1, 4, 7, 0, 3, 6, 20, 23, 26, 19, 22, 25, 18, 21, 24, 47, 50, 53, 46, 49, 52, 45, 48, 51, 42, 39, 36, 43, 40, 37, 44, 41, 38, 11, 14, 17, 10, 13, 16, 9, 12, 15),
-    "z2" : (53, 52, 51, 50, 49, 48, 47, 46, 45, 35, 34, 33, 32, 31, 30, 29, 28, 27, 26, 25, 24, 23, 22, 21, 20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 44, 43, 42, 41, 40, 39, 38, 37, 36, 8, 7, 6, 5, 4, 3, 2, 1, 0),
-}
 
 
 def print_mem_stats(desc):
@@ -612,9 +610,6 @@ class RubiksCube333(object):
         """
         Apply `step` to the cube and append `step` to our solution list
         """
-        ref_swaps_333 = swaps_333
-        ref_state = self.state
-        ref_state_scratchpad = self.state_scratchpad
 
         # On a laptop the following list comp is the fastest way to do the rotate. This involves
         # allocating memory for the new list everytime though.  That malloc is about 600x slower
@@ -622,66 +617,10 @@ class RubiksCube333(object):
         #
         # self.state = [ref_state[x] for x in ref_swaps_333[step]]
 
-        # dwalton
         # So what we do instead is use a scratchpad bytearray to do the rotate without
         # any mallocs
-        for (i, x) in enumerate(ref_swaps_333[step]):
-            ref_state_scratchpad[i] = ref_state[x]
-
-        ref_state[0] = ref_state_scratchpad[0]
-        ref_state[1] = ref_state_scratchpad[1]
-        ref_state[2] = ref_state_scratchpad[2]
-        ref_state[3] = ref_state_scratchpad[3]
-        ref_state[4] = ref_state_scratchpad[4]
-        ref_state[5] = ref_state_scratchpad[5]
-        ref_state[6] = ref_state_scratchpad[6]
-        ref_state[7] = ref_state_scratchpad[7]
-        ref_state[8] = ref_state_scratchpad[8]
-        ref_state[9] = ref_state_scratchpad[9]
-        ref_state[10] = ref_state_scratchpad[10]
-        ref_state[11] = ref_state_scratchpad[11]
-        ref_state[12] = ref_state_scratchpad[12]
-        ref_state[13] = ref_state_scratchpad[13]
-        ref_state[14] = ref_state_scratchpad[14]
-        ref_state[15] = ref_state_scratchpad[15]
-        ref_state[16] = ref_state_scratchpad[16]
-        ref_state[17] = ref_state_scratchpad[17]
-        ref_state[18] = ref_state_scratchpad[18]
-        ref_state[19] = ref_state_scratchpad[19]
-        ref_state[20] = ref_state_scratchpad[20]
-        ref_state[21] = ref_state_scratchpad[21]
-        ref_state[22] = ref_state_scratchpad[22]
-        ref_state[23] = ref_state_scratchpad[23]
-        ref_state[24] = ref_state_scratchpad[24]
-        ref_state[25] = ref_state_scratchpad[25]
-        ref_state[26] = ref_state_scratchpad[26]
-        ref_state[27] = ref_state_scratchpad[27]
-        ref_state[28] = ref_state_scratchpad[28]
-        ref_state[29] = ref_state_scratchpad[29]
-        ref_state[30] = ref_state_scratchpad[30]
-        ref_state[31] = ref_state_scratchpad[31]
-        ref_state[32] = ref_state_scratchpad[32]
-        ref_state[33] = ref_state_scratchpad[33]
-        ref_state[34] = ref_state_scratchpad[34]
-        ref_state[35] = ref_state_scratchpad[35]
-        ref_state[36] = ref_state_scratchpad[36]
-        ref_state[37] = ref_state_scratchpad[37]
-        ref_state[38] = ref_state_scratchpad[38]
-        ref_state[39] = ref_state_scratchpad[39]
-        ref_state[40] = ref_state_scratchpad[40]
-        ref_state[41] = ref_state_scratchpad[41]
-        ref_state[42] = ref_state_scratchpad[42]
-        ref_state[43] = ref_state_scratchpad[43]
-        ref_state[44] = ref_state_scratchpad[44]
-        ref_state[45] = ref_state_scratchpad[45]
-        ref_state[46] = ref_state_scratchpad[46]
-        ref_state[47] = ref_state_scratchpad[47]
-        ref_state[48] = ref_state_scratchpad[48]
-        ref_state[49] = ref_state_scratchpad[49]
-        ref_state[50] = ref_state_scratchpad[50]
-        ref_state[51] = ref_state_scratchpad[51]
-        ref_state[52] = ref_state_scratchpad[52]
-        ref_state[53] = ref_state_scratchpad[53]
+        rotate_function = rotate_map.get(step)
+        rotate_function(self.state, self.state_scratchpad)
 
         self.solution.append(step)
 
---
+++ utils/time-cube-solve.py
@@ -0,0 +1,28 @@
+
+import time
+from rubikscubesolvermicropython.cube import RubiksCube333
+
+cube_state = "RRBBUFBFBRLRRRFRDDURUBFBBRFLUDUDFLLFFLLLLDFBDDDUUBDLUU"
+cube = RubiksCube333(cube_state, 'URFDLB')
+cube.load_tables()
+
+def solve_cube(count):
+    ref_cube = cube
+    r = range(count)
+
+    for i in r:
+        ref_cube.solve()
+        ref_cube.re_init()
+
+def time_it(f, n):
+    t0 = time.ticks_us()
+    f(n)
+    t1 = time.ticks_us()
+    dt = time.ticks_diff(t1, t0)
+    fmt = "{:5.3f} sec, {:6.3f} usec/solve, {:8.4f} solves/sec"
+    print(fmt.format(dt * 1e-6, dt / n, n / dt * 1e3))
+
+
+time_it(solve_cube, 1000)
+
+# baseline 0.08 solves/sec
