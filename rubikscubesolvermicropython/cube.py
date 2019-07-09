cc6387dece78a1dbca705844999043b8bd4a71f6
Daniel Walton <dwalton76@gmail.com>
2019-07-08 20:01:06 -0400

    Added rubikscubesolvermicropython/profile.py

--- rubikscubesolvermicropython/cube.py
+++ rubikscubesolvermicropython/cube.py
@@ -110,28 +110,6 @@ def print_mem_stats(desc):
     print('{} free: {} allocated: {}'.format(desc, gc.mem_free(), gc.mem_alloc()))
 
 
-profile_stats_time = {}
-profile_stats_calls = {}
-
-def timed_function(f, *args, **kwargs):
-    myname = str(f).split(' ')[1]
-
-    def new_func(*args, **kwargs):
-        t = utime.ticks_us()
-        result = f(*args, **kwargs)
-
-        if myname not in profile_stats_time:
-            profile_stats_time[myname] = 0
-            profile_stats_calls[myname] = 0
-
-        profile_stats_time[myname] += utime.ticks_diff(utime.ticks_us(), t)
-        profile_stats_calls[myname] += 1
-
-        return result
-
-    return new_func
-
-
 # @timed_function
 def get_lines_in_file(file_data, line_width, line_index, lines_to_get):
     """
